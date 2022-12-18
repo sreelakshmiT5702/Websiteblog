96b8d836208b81154b89da0335a987a5c93c6f4b
nxvya <navyamol761@gmail.com>
2022-12-18 10:03:23 +0530

    fourth commit-backend

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+// const fileupload = require('express-fileupload');
+
+// <!--public folder path to this variable---!>
+
+let initial_path = path.join(__dirname, "public/uploads");
+
+// setup the server
+const app = express();
+app.use(express.static(initial_path));
+// app.use(fileupload());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(initial_path, "home.html"));
+})
+
+app.listen("3000", () => {
+    console.log('listening......');
+})
